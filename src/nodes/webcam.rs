ccff1531f56f15259919913c47f1afcb67a5abc3
ProfBits <53108880+ProfBits@users.noreply.github.com>
2023-10-30 12:14:08 +0100

    Move implementation into a flow-rs node
    Moved the implmentation of the the PoC to a real flow-rs node
    and split the logic into on_ready and on_update parts with
    error handling.

--- src/nodes/webcam.rs
+++ src/nodes/webcam.rs
@@ -1,84 +1,70 @@
-use std::fmt::Debug;
-
+use core::result::Result::Ok;
 use flowrs::{
-    connection::{Output, Input},
-    node::{ChangeObserver, ReadyError, Node, UpdateError},
+    connection::Output,
+    node::{ChangeObserver, ReadyError, Node, UpdateError, SendError},
 };
 use flowrs::RuntimeConnectable;
+use image::DynamicImage;
 use nokhwa::{utils::{CameraIndex, RequestedFormat, RequestedFormatType}, pixel_format::RgbFormat, Camera};
 use serde::{Deserialize, Serialize};
 
 #[derive(RuntimeConnectable, Deserialize, Serialize)]
-pub struct WebcamNode<I>
-where
-    I: Clone,
+pub struct WebcamNode
 {
-    #[input]
-    pub input: Input<I>,
+    camera: Option<Camera>,
     #[output]
-    pub output: Output<u32>,
+    pub output: Output<DynamicImage>,
 }
 
-impl<I> WebcamNode<I>
-where
-    I: Clone,
+impl WebcamNode
 {
     pub fn new(change_observer: Option<&ChangeObserver>) -> Self {
         Self {
-            input: Input::new(),
+            camera: None,
             output: Output::new(change_observer),
         }
     }
 }
 
-impl<I> Node for WebcamNode<I>
-where
-    I: Clone + Debug + Send,
+impl Node for WebcamNode
 {
+    fn on_ready(&self) -> Result<(), ReadyError>{
+        let index = CameraIndex::Index(0); 
+        let requested = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
+        let camera = Camera::new(index, requested);
+        
+        match camera {
+            Ok(cam) => {
+                self.camera = Some(cam);
+                let _ = cam.frame();
+                Ok(())
+            },
+            Err(err) => ReadyError::Other(err)
+        }
+    }
+
     fn on_update(&mut self) -> Result<(), UpdateError> {
-        // first camera in system
-        let index = CameraIndex::Index(0); 
-        // request the absolute highest resolution CameraFormat that can be decoded to RGB.
-        let requested = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
-        // make the camera
-        let mut camera = Camera::new(index, requested).unwrap();
-
-        // get a frame
-        let frame = camera.frame().unwrap();
-        println!("Captured Single Frame of {}", frame.buffer().len());
-        // decode into an ImageBuffer
-        let decoded = frame.decode_image::<RgbFormat>().unwrap();
-        println!("Decoded Frame of {}", decoded.len());
-        Ok(())
+        
+        let no_cam_err = "No camera available";
+        match self.camera {
+            None => Err(UpdateError::SendError { message: *no_cam_err }),
+            Some(cam) => {
+                let r_frame = cam.frame();
+                match r_frame {
+                    Err(err) => Err(UpdateError::Other(err)),
+                    Ok(frame) => {
+                        let r_decode = frame.decode_image::<RgbFormat>();
+                        match r_decode {
+                            Err(err) => Err(UpdateError::Other(err)),
+                            Ok(decoded) => {
+                                let dyn_img = DynamicImage::ImageRgb8(decoded);
+                                self.output.clone().send(dyn_img).map_err(|e| UpdateError::Other(e.into()))?;
+                                Ok(())
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        // first camera in system
-        let index = CameraIndex::Index(0); 
-        // request the absolute highest resolution CameraFormat that can be decoded to RGB.
-        let requested = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
-        // make the camera
-        let mut camera = Camera::new(index, requested).unwrap();
-
-        let _ = camera.open_stream();
-
-        // get a frame
-        let frame = camera.frame().unwrap();
-        println!("Captured Single Frame of {}", frame.buffer().len());
-        // decode into an ImageBuffer
-        let decoded = frame.decode_image::<RgbFormat>().unwrap();
-        println!("Decoded Frame of {}", decoded.len());
-
-        decoded.save("test.png").unwrap();
-
-        assert!(decoded.len() > 0);
-    }
-
-
 }
