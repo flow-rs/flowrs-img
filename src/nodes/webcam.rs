9d35184597f3dcbd9788b449be20e3b22005f43c
Ron Heller <ron.heller@work-microwave.com>
2023-11-08 13:23:31 +0100

    fix Send/Serialize/Deserialize/Errorhandling

--- src/nodes/webcam.rs
+++ src/nodes/webcam.rs
@@ -1,23 +1,25 @@
 use core::result::Result::Ok;
+use flowrs::RuntimeConnectable;
 use flowrs::{
     connection::Output,
-    node::{ChangeObserver, ReadyError, Node, UpdateError, SendError},
+    node::{ChangeObserver, Node, ReadyError, SendError, UpdateError},
 };
-use flowrs::RuntimeConnectable;
 use image::DynamicImage;
-use nokhwa::{utils::{CameraIndex, RequestedFormat, RequestedFormatType}, pixel_format::RgbFormat, Camera};
+use nokhwa::{
+    pixel_format::RgbFormat,
+    utils::{CameraIndex, RequestedFormat, RequestedFormatType},
+    Camera,
+};
 use serde::{Deserialize, Serialize};
 
-#[derive(RuntimeConnectable, Deserialize, Serialize)]
-pub struct WebcamNode
-{
+#[derive(RuntimeConnectable)]
+pub struct WebcamNode {
     camera: Option<Camera>,
     #[output]
     pub output: Output<DynamicImage>,
 }
 
-impl WebcamNode
-{
+impl WebcamNode {
     pub fn new(change_observer: Option<&ChangeObserver>) -> Self {
         Self {
             camera: None,
@@ -26,44 +28,48 @@ impl WebcamNode
     }
 }
 
-impl Node for WebcamNode
-{
-    fn on_ready(&self) -> Result<(), ReadyError>{
-        let index = CameraIndex::Index(0); 
-        let requested = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
+impl Node for WebcamNode {
+    fn on_ready(&self) -> Result<(), ReadyError> {
+        let index = CameraIndex::Index(0);
+        let requested =
+            RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
         let camera = Camera::new(index, requested);
-        
+
         match camera {
-            Ok(cam) => {
-                let testFrame = cam.frame();
-                match testFrame {
-                    Err(err) => ReadyError::Other(err),
+            Ok(mut cam) => {
+                let test_frame = cam.frame();
+                match test_frame {
+                    Err(err) => Err(ReadyError::Other(err.into())),
                     Ok(_) => {
-                        self.camera = Some(cam);
+                        self.camera = Some(cam); // Wrap Camera in Arc<Mutex>
                         Ok(())
                     }
                 }
-            },
-            Err(err) => ReadyError::Other(err)
+            }
+            Err(err) => Err(ReadyError::Other(err.into())),
         }
     }
 
     fn on_update(&mut self) -> Result<(), UpdateError> {
-        
         let no_cam_err = "No camera available";
-        match self.camera {
-            None => Err(UpdateError::SendError { message: *no_cam_err }),
-            Some(cam) => {
+        match &self.camera {
+            None => Err(UpdateError::SendError {
+                message: no_cam_err.to_string(),
+            }),
+            Some(mut cam) => {
                 let r_frame = cam.frame();
                 match r_frame {
-                    Err(err) => UpdateError::Other(err),
+                    Err(err) => Err(UpdateError::Other(err.into())),
                     Ok(frame) => {
                         let r_decode = frame.decode_image::<RgbFormat>();
                         match r_decode {
-                            Err(err) => UpdateError::Other(err),
+                            Err(err) => Err(UpdateError::Other(err.into())),
                             Ok(decoded) => {
                                 let dyn_img = DynamicImage::ImageRgb8(decoded);
-                                self.output.clone().send(dyn_img).map_err(|e| UpdateError::Other(e.into()))?;
+                                self.output
+                                    .clone()
+                                    .send(dyn_img)
+                                    .map_err(|e| UpdateError::Other(e.into()))?;
                                 Ok(())
                             }
                         }
