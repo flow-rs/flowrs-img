b2205927baa32d89c732d76824c39924a0793091
ProfBits <53108880+ProfBits@users.noreply.github.com>
2023-11-13 15:13:43 +0100

    Implement native test for WebcamNode
    This implements a basic test of the WebcamNode in system.
    It is only tested if a frame can be successfully retrived and
    that the frame has some width greater than 0.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ mod nodes;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 pub use self::nodes::transform;
+pub use self::nodes::webcam;
---
+++ tests/nodes/webcam.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod nodes {
+    use flowrs::connection::{connect, Edge};
+    use flowrs::node::{Node, ChangeObserver, ReceiveError};
+    use flowrs_img::webcam::WebcamNode;
+
+
+    #[test]
+    fn should_return_some_frame() -> Result<(), ReceiveError> {
+        let change_observer: ChangeObserver = ChangeObserver::new(); 
+        let mut webcam = WebcamNode::new(Some(&change_observer));
+
+        let mock_output = Edge::new();
+        connect(webcam.output.clone(), mock_output.clone());
+        let _ = webcam.on_update();
+
+        let actual_image = mock_output.next();
+        match actual_image {
+            Ok(dyn_img) => Ok(assert!(dyn_img.width() > 0)),
+            Err(err) => Err(err),
+        }
+    }
+}
